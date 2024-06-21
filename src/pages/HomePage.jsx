53996ef320b4cf312ebf62e4a192e8fcc3ed38ba
Zachary Kohs <zachary.c.kohs@gmail.com>
2024-06-21 15:11:45 -0400

    Made the home page show the Profile Form if the user has a profile and the counters if not

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,41 @@
+import { useState, useEffect } from "react"
 import CountersContainer from "../components/CountersContainer"
 import HeaderText from "../components/HeaderText"
+import ProfileForm from "../components/ProfileForm"
+import supabase from "../utils/supabase"
 
 const HomePage = ({currentUser, counterQuantity, changeCounterQuantity}) => {
+  const [profileExists, setProfileExists] = useState(false)
+  
+  const checkProfile = async () => {
+    if (currentUser) {
+      let { data: profiles, error } = await supabase
+        .from('profiles')
+        .select()
+        .eq('id', currentUser.id)
+      if(error) {
+        console.log(error)
+      } else {
+        if (profiles.length) {
+          setProfileExists(true)
+        }
+      }
+    }
+  }
+
+  checkProfile()
+
   return (
     <div>
-      <HeaderText currentUser={currentUser} />
-      <CountersContainer currentUser={currentUser} counterQuantity={counterQuantity} changeCounterQuantity={changeCounterQuantity} />
+      {profileExists ? 
+        <div>
+          <HeaderText currentUser={currentUser} />
+          <CountersContainer currentUser={currentUser} counterQuantity={counterQuantity} changeCounterQuantity={changeCounterQuantity} />
+        </div>
+        :
+        <ProfileForm currentUser={currentUser} />
+      }
+      
     </div>
   )
 }
